27099964e65cdfc23b40ff0167a06537143699a5
MvdS <git@vandersluys.nl>
2019-02-23 11:04:45 +0100

    vsop: move code to function readVSOP()

--- VSOP/vsop.py
+++ VSOP/vsop.py
@@ -3,30 +3,35 @@
 import numpy as np
 import fortranformat as ff
 
-inFile=open('VSOP87D.jup','r')
-#inFile=open('VSOP87D.ura','r')
-
-formatHeader = ff.FortranRecordReader('(40x,I3, 16x,I1,I8)')         # Block header format
-formatBody   = ff.FortranRecordReader('(79x,F18.11,F14.11,F20.11)')  # Block body format
-
-lon=[]; lat=[]; rad=[]
-
-for iBlock in range(3*6):  # 3 variables (l,b,r), up to 6 powers (0-5)
-    line = inFile.readline()
-    var,power,nTerm = formatHeader.read(line)
-    #print(var,power,nTerm)
-    if line == '': break  # EoF
+def readVSOP(fileName):
+    inFile = open(fileName,'r')
     
-    for iLine in range(nTerm):
+    formatHeader = ff.FortranRecordReader('(40x,I3, 16x,I1,I8)')         # Block header format
+    formatBody   = ff.FortranRecordReader('(79x,F18.11,F14.11,F20.11)')  # Block body format
+    
+    lon=[]; lat=[]; rad=[]
+    
+    for iBlock in range(3*6):  # 3 variables (l,b,r), up to 6 powers (0-5)
         line = inFile.readline()
-        a,b,c = formatBody.read(line)
-        #print(iLine, var,power, a,b,c)
+        var,power,nTerm = formatHeader.read(line)
+        #print(var,power,nTerm)
+        if line == '': break  # EoF
         
-        if var == 1: lon.append([power, a,b,c])  # var=1: ecliptic longitude
-        if var == 2: lat.append([power, a,b,c])  # var=2: ecliptic latitude
-        if var == 3: rad.append([power, a,b,c])  # var=3: radial distance
-        
+        for iLine in range(nTerm):
+            line = inFile.readline()
+            a,b,c = formatBody.read(line)
+            #print(iLine, var,power, a,b,c)
+            
+            if var == 1: lon.append([power, a,b,c])  # var=1: ecliptic longitude
+            if var == 2: lat.append([power, a,b,c])  # var=2: ecliptic latitude
+            if var == 3: rad.append([power, a,b,c])  # var=3: radial distance
 
+    return lon,lat,rad
+
+
+# Read a VSOP data file:
+lon,lat,rad = readVSOP('VSOP87D.jup')
+#lon,lat,rad = readVSOP('VSOP87D.ura')
 
 print(np.shape(lon))
 print(lon[0])
@@ -39,8 +44,3 @@ print(lat[-1])
 print(np.shape(rad))
 print(rad[0])
 print(rad[-1])
-exit()
-
-print(inData[0:10])
-
-print(np.shape(inData))
