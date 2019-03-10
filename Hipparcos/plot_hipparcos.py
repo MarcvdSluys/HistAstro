aa5c38f39456b518d4db86e1a5471caad56f97d6
MvdS <git@vandersluys.nl>
2019-03-10 16:24:24 +0100

    Hipparcos: add functions precessHip() for precession and properMotion() to compute proper motion

--- Hipparcos/plot_hipparcos.py
+++ Hipparcos/plot_hipparcos.py
@@ -9,22 +9,47 @@ import matplotlib.pyplot as plt
 pi    = m.pi
 pi2   = 2*pi
 r2d   = m.degrees(1)  # Radians to degrees
+d2r   = 1.0/r2d       # Degrees to radians
 #r2h  = r2d/15
-d2r   = 1.0/r2d          # Degrees to radians
-mas2r = d2r/3.6e6      # Milliarcseconds to radians
-h2r   = d2r*15          # Hours to radians
+h2r   = d2r*15        # Hours to radians
+as2r  = d2r/3.6e3     # Arcseconds to radians
+mas2r = as2r/1000.0   # Milliarcseconds to radians
 
 def eq2ecl(ra,dec, eps):
+    """Convert equatorial coordinates to ecliptical"""
     lon = np.arctan2( np.sin(ra)  * m.cos(eps) + np.tan(dec) * m.sin(eps),  np.cos(ra) ) % pi2
     lat =  np.arcsin( np.sin(dec) * m.cos(eps) - np.cos(dec) * m.sin(eps) * np.sin(ra) )
     return lon,lat
 
 def par2horiz(ha,dec, phi):
+    """Convert parallactic coordinates to horizontal"""
     az  = np.arctan2( np.sin(ha),   np.cos(ha) * m.sin(phi) - np.tan(dec) * m.cos(phi) ) % pi2
     alt = np.arcsin(  np.sin(dec) * m.sin(phi) + np.cos(ha) * np.cos(dec) * m.cos(phi) )
     return az,alt
 
+def precessHip(jd, ra,dec):
+    """Compute precession in equatorial coordinates from the Hipparcos epoch (J2000) to the specified JD"""
+    tJC = (jd - 2451545.0)/36525  # Time in Julian centuries since J2000.0
+    tJC2 = tJC**2
+    tJC3 = tJC*tJC2
     
+    zeta  = (2306.2181*tJC + 0.30188*tJC2 + 0.017998*tJC3)*as2r
+    z     = (2306.2181*tJC + 1.09468*tJC2 + 0.018203*tJC3)*as2r
+    theta = (2004.3109*tJC - 0.42665*tJC2 - 0.041833*tJC3)*as2r
+    
+    raNew  = np.arctan2( np.sin(ra + zeta) * np.cos(dec),  np.cos(ra + zeta) * m.cos(theta) * np.cos(dec) - m.sin(theta) * np.sin(dec) ) + z
+    decNew = np.arcsin( np.cos(ra + zeta) * m.sin(theta) * np.cos(dec)  +  m.cos(theta) * np.sin(dec) )
+    return raNew,decNew
+
+def properMotion(startYear,targetYear, ra,dec, pma,pmd):
+    """Compute the proper motion from startYear to targetYear for the positions given in (numpy arrays) ra and dec and proper motions in pma,pmd"""
+    dt = targetYear - startYear
+    raOld  = ra  + pma*dt / np.cos(dec)
+    decOld = dec + pmd*dt
+    return raOld,decOld
+
+
+
 # Time the execution:
 import time
 t0 = time.perf_counter() 
@@ -63,12 +88,8 @@ pmd = hip[:,5]*mas2r                     # pmDec, mas/yr -> rad/yr
 
 # Correct for proper motion:
 t3 = time.perf_counter() 
-startEpoche  = 1992.25
-targetEpoche = -1500
-dt = targetEpoche - startEpoche
+raOld,decOld = properMotion(1991.25,-1500, ra,dec, pma,pmd)
 
-raOld  = ra  + pma*dt / np.cos(dec)
-decOld = dec + pmd*dt
 
 
 t4 = time.perf_counter() 
