8bd3efe9ee51aa6646fd9222da1f4a8790755167
MvdS <git@vandersluys.nl>
2019-04-30 10:49:33 +0200

    histastro/coordinates: add function ecl2eq() to convert sky positions from the ecliptical to the equatorial coordinate system

--- histastro/coordinates.py
+++ histastro/coordinates.py
@@ -23,6 +23,7 @@ def obliquity(JD):
     eps = 0.409092804 - 2.269655e-4*tJC - 2.86e-9*tJC**2 + 8.78967e-9*tJC**3  # Checked
     return eps
 
+
 def eq2ecl(ra,dec, eps):
     """Convert equatorial coordinates to ecliptical"""
     lon = np.arctan2( np.sin(ra)  * m.cos(eps) + np.tan(dec) * m.sin(eps),  np.cos(ra) ) % pi2
@@ -30,11 +31,20 @@ def eq2ecl(ra,dec, eps):
     return lon,lat
 
 
+def ecl2eq(lon,lat, eps):
+    """Convert (geocentric) spherical ecliptical coordinates lon, lat (and eps) to spherical equatorial coordinates RA, Dec.
+    See Expl. Supl. t.t. Astronimical Almanac 3rd Ed, Eq.14.43"""
+    ra  = np.arctan2( np.sin(lon) * np.cos(eps)  -  np.tan(lat) * np.sin(eps),  np.cos(lon) ) % pi2
+    dec =  np.arcsin( np.sin(lat) * np.cos(eps)  +  np.cos(lat) * np.sin(eps) * np.sin(lon) )
+    return ra,dec
+
+
 def par2horiz(ha,dec, phi):
     """Convert parallactic coordinates to horizontal"""
     az  = np.arctan2( np.sin(ha),   np.cos(ha) * m.sin(phi) - np.tan(dec) * m.cos(phi) ) % pi2
     alt = np.arcsin(  np.sin(dec) * m.sin(phi) + np.cos(ha) * np.cos(dec) * m.cos(phi) )
     return az,alt
+
 
 def properMotion(startJD,targetJD, ra,dec, pma,pmd):
     """Compute the proper motion from startJD to targetJD for the positions given in (numpy arrays) ra and dec
