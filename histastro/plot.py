a3a9e4eb34a738892072ba17ae512d18905a9e1b
MvdS <git@vandersluys.nl>
2019-04-30 15:27:56 +0200

    ex04: compute Jupiter's magnitude and plot planet with correct size

--- histastro/plot.py
+++ histastro/plot.py
@@ -1,8 +1,8 @@
 """HistAstro plot-related functions"""
 
-def mag2size(Mlim, mag):
+def mag2size(Mlim, mag, scale=1):
     """Convert magnitudes (mag) to disc area ('size') for plotting with pyplot.scatter(), given a magnitude limit Mlim"""
     
-    size = 30*(0.5 + (Mlim-mag)/3.0)**2     # Disc area as a function of magnitude
+    size = scale * 30*(0.5 + (Mlim-mag)/3.0)**2     # Disc area as a function of magnitude
     return size
 
