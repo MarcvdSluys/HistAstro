3dbae6ad1d2cb68e471be168654a22b3855ee668
MvdS <git@vandersluys.nl>
2019-05-19 19:21:39 +0200

    HistAstro/moon: compute dLon/dLat separately and in radians.  Print intermediate values for verification.

--- histastro/moon.py
+++ histastro/moon.py
@@ -2,19 +2,22 @@
 
 import math as m
 import numpy as np
-from histastro.constants import pi2,d2r,jd2000,moonRad
+from histastro.constants import pi2,d2r,r2d,jd2000,moonRad
 
 
 def readData(inFile):
-    """Read the periodic terms for the ELP82B theory, selected by Meeus"""
+    """Read the periodic terms for the ELP82B theory, selected by Meeus and return them in two arrays: one for
+    longitude and distance, and one for latitude.
+    """
+    
     lrTerms = np.genfromtxt(inFile, delimiter=',', skip_header=1,  max_rows=60)  # Longitude and radius (6 columns: 4 args, 2 coefs)
     bTerms  = np.genfromtxt(inFile, delimiter=',', skip_header=61, max_rows=60)  # Latitude (5 columns: 4 args, 1 coef)
     return lrTerms,bTerms
 
 
 def compute_lbr(jde, lrTerms,bTerms):
-    """
-    Compute the geocentric ecliptic coordinates of the Moon for the equinox of date, from the JDE
+    """Compute the geocentric ecliptic coordinates of the Moon for the equinox of date, from the JDE (and ELP82
+    terms) provided.
     """
     
     tjc   = (jde-jd2000)/36525  # Julian Centuries after 2000.0 in dynamical time
@@ -46,7 +49,7 @@ def compute_lbr(jde, lrTerms,bTerms):
         lon  += m.sin(argl) * lrTerms[iLine,4] * e**(abs(lrTerms[iLine,1]))
         lat  += m.sin(argb) *  bTerms[iLine,4] * e**(abs(bTerms[iLine,1]))
         dist += m.cos(argl) * lrTerms[iLine,5] * e**(abs(lrTerms[iLine,1]))
-    
+
     
     # Perturbations by other planets, and flattening of the Earth:
     # Meeus, p.338:
@@ -55,21 +58,49 @@ def compute_lbr(jde, lrTerms,bTerms):
     a3 = (5.4707345 + 8399.6847253 * tjc) % pi2
     
     # Meeus, p.342:
-    lon += (3958*m.sin(a1) + 1962*m.sin(lm-f) + 318*m.sin(a2)) * d2r * 1e-6
-    lat += (2235*m.sin(lm) + 382*m.sin(a3) + 175*m.sin(a1-f) + 175*m.sin(a1+f) + 127*m.sin(lm-mm) - 115*m.sin(lm+mm)) * d2r * 1e-6
+    #dlon = (3958*m.sin(a1) + 1962*m.sin(lm-f) + 318*m.sin(a2)) * d2r * 1e-6
+    #dlat = (-2235*m.sin(lm) + 382*m.sin(a3) + 175*m.sin(a1-f) + 175*m.sin(a1+f) + 127*m.sin(lm-mm) - 115*m.sin(lm+mm)) * d2r * 1e-6
+    dlon =  6.908e-5*m.sin(a1) + 3.4243e-5*m.sin(lm-f) + 5.55e-6*m.sin(a2)
+    dlat = -3.9008e-5*m.sin(lm) + 6.667e-6*m.sin(a3) + 3.0543e-6*m.sin(a1-f) + 3.0543e-6*m.sin(a1+f) + 2.2166e-6*m.sin(lm-mm) - 2.007e-6*m.sin(lm+mm)
     
     
     # Compute nutation in longitude:
-    #omg  = 2.18243858558 - 33.7570459367*tjc + 3.6142278e-5*tjc2 + 3.87850944888e-8*tjc3   # Moon's mean lon. of asc.node, Meeus p.144
-    #ls   = 4.89506386655 + 62.84528862*tjc                                                 # Mean long. Sun, Meeus p.144
-    #dpsi = -8.338795e-5*m.sin(omg) - 6.39954e-6*m.sin(2*ls) - 1.115e-6*m.sin(2*lm) + 1.018e-6*m.sin(2*omg)
-    dpsi = 0
+    omg  = 2.18243858558 - 33.7570459367*tjc + 3.6142278e-5*tjc2 + 3.87850944888e-8*tjc3   # Moon's mean lon. of asc.node, Meeus p.144
+    ls   = 4.89506386655 + 62.84528862*tjc                                                 # Mean long. Sun, Meeus p.144
+    dpsi = -8.338795e-5*m.sin(omg) - 6.39954e-6*m.sin(2*ls) - 1.115e-6*m.sin(2*lm) + 1.018e-6*m.sin(2*omg)
+    #dpsi = 0
     
     # Add mean values:
-    lon  = (lon + lm + dpsi) % pi2
+    lon  = (lon + dlon + lm + dpsi) % pi2
+    lat  = lat + dlat
     dist = dist + 385000.56  # in km
     
     diam = 2*m.atan(moonRad/dist)
+    
+    print()
+    print('%10s  %25.15f' % ('jde:  ', jde) )
+    print('%10s  %25.15f' % ('tjc:  ', tjc) )
+    print()
+    print('%10s  %25.15f' % ('lm:  ', lm*r2d) )
+    print('%10s  %25.15f' % ('d:   ', d*r2d) )
+    print('%10s  %25.15f' % ('ms:  ', ms*r2d) )
+    print('%10s  %25.15f' % ('mm:  ', mm*r2d) )
+    print('%10s  %25.15f' % ('f:   ', f*r2d) )
+    print('%10s  %25.15f' % ('e:   ', e) )
+    print()
+    print('%10s  %25.15f' % ('a1:  ', a1*r2d) )
+    print('%10s  %25.15f' % ('a2:  ', a2*r2d) )
+    print('%10s  %25.15f' % ('a3:  ', a3*r2d) )
+    print()
+    print('%10s  %25.15f' % ('dlon: ', dlon*r2d) )
+    print('%10s  %25.15f' % ('dlat: ', dlat*r2d) )
+    print()
+    print('%10s  %25.15f' % ('lon:  ', lon*r2d) )
+    print('%10s  %25.15f' % ('lat:  ', lat*r2d) )
+    print('%10s  %25.15f' % ('dist:  ', dist) )
+    print('%10s  %25.15f' % ('diam:  ', diam*r2d) )
+    print()
+#    print('%10s  %25.15f' % ('x:  ', x*r2d) )
     
     return lon,lat,dist,diam
 
