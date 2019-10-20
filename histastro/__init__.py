a2d68d50e55e318b1ab46507458b78859fd3d162
MvdS <git@vandersluys.nl>
2019-10-20 16:09:59 +0200

    Add copyright and licence statements to all source files

--- histastro/__init__.py
+++ histastro/__init__.py
@@ -1,10 +1,27 @@
+#  Copyright (c) 2019  Marc van der Sluys - marc.vandersluys.nl
+#   
+#  This file is part of the HistAstro Python package,
+#  see: http://astro.ru.nl/~sluys/HistAstro/
+#   
+#  This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#  
+#  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License along with this code.  If not, see
+#  <http://www.gnu.org/licenses/>.
+
+
 """HistAstro module
 
-HistAstro is a Python package for basic historical-astronomy calculations of Sun, Moon and planets.  These
-pages contain the API documentation.  For more information on the Python package, source code and data files,
-see the [HistAstro homepage](http://astro.ru.nl/~sluys/HistAstro/).
+HistAstro is a Python package for basic historical-astronomy calculations of Sun, Moon and planets, which can
+be used under the conditions of the GPLv3 licence.  These pages contain the API documentation.  For more
+information on the Python package, licence, source code and data files, see the [HistAstro
+homepage](http://astro.ru.nl/~sluys/HistAstro/).
 
 """
+
 
 name = "histastro"
 
