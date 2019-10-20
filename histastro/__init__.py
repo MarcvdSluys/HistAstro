e1440ce21ff372632ed4553ac3d25c41e4bfd455
MvdS <git@vandersluys.nl>
2019-10-20 14:24:56 +0200

    Add __init__.py to repo

---
+++ histastro/__init__.py
@@ -0,0 +1,10 @@
+"""HistAstro module
+
+HistAstro is a Python package for basic historical-astronomy calculations of Sun, Moon and planets.  These
+pages contain the API documentation.  For more information on the Python package, source code and data files,
+see the [HistAstro homepage](http://astro.ru.nl/~sluys/HistAstro/).
+
+"""
+
+name = "histastro"
+
